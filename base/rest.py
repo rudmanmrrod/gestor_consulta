e52505cf266365635e33fd69d5c5739ab6333f3d
Argenis Osorio <argenisosorio580@hotmail.com>
2017-08-16 03:05:17 -0400

    Documentado el código

--- base/rest.py
+++ base/rest.py
@@ -4,10 +4,10 @@ Sistema de Consulta Pública
 
 Copyleft (@) 2017 CENDITEL nodo Mérida - https://planificacion.cenditel.gob.ve/trac/wiki/ModeladoTopicos_2017
 """
-## @package consulta.rest
+## @package base.rest
 #
 # ViewSet (vistas de los rest) para el rest framework
-# @author Rodrigo Boet (rboet at cenditel.gob.ve)
+# @author Argenis Osorio (aosorio at cenditel.gob.ve)
 # @author <a href='http://www.cenditel.gob.ve'>Centro Nacional de Desarrollo e Investigación en Tecnologías Libres
 # (CENDITEL) nodo Mérida - Venezuela</a>
 # @copyright <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GNU Public License versión 2 (GPLv2)</a>
@@ -18,8 +18,14 @@ from rest_framework import viewsets
 
 
 class EntidadViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
+    """!
+    Clase que gestiona los datos rest de las Entidades
+
+    @author Argenis Osorio (aosorio at cenditel.gob.ve)
+    @copyright <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GNU Public License versión 2 (GPLv2)</a>
+    @date 16-08-2017
+    @version 1.0.0
     """
     queryset = Entidad.objects.all()
     serializer_class = EntidadSerializer
+    http_method_names = ['get','head']
--- base/serializers.py
+++ base/serializers.py
@@ -1,9 +1,31 @@
 # -*- coding: utf-8 -*-
+"""
+Sistema de Consulta Pública
+
+Copyleft (@) 2017 CENDITEL nodo Mérida - https://planificacion.cenditel.gob.ve/trac/wiki/ModeladoTopicos_2017
+"""
+## @package base.serializers
+#
+# Serializadores de los modelos de las Entidades para el rest framework
+# @author Argenis Osorio (aosorio at cenditel.gob.ve)
+# @author <a href='http://www.cenditel.gob.ve'>Centro Nacional de Desarrollo e Investigación en Tecnologías Libres
+# (CENDITEL) nodo Mérida - Venezuela</a>
+# @copyright <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GNU Public License versión 2 (GPLv2)</a>
+# @version 1.0
 from rest_framework import serializers
 from .models import Entidad
 
 
 class EntidadSerializer(serializers.HyperlinkedModelSerializer):
+    """!
+    Metodo que permite serializar el modelo Entidad
+
+    @author Argenis Osorio (aosorio at cenditel.gob.ve)
+    @copyright GNU/GPLv2
+    @date 16-08-2017
+    @param serializers.HyperlinkedModelSerializer <b>{object}</b> Objeto del serializer
+    @return Retorna los datos de contexto
+    """
 
     class Meta:
         model = Entidad
