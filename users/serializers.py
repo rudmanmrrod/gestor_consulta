d821e95bb4dd7f7c5a2b1ecbd0fbef34ea435c28
Argenis Osorio <argenisosorio580@hotmail.com>
2017-08-17 08:21:42 -0400

    Arreglado error de indentación

--- users/serializers.py
+++ users/serializers.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-	"""!
+    """!
     Metodo que permite serializar el modelo User
 
     @author Argenis Osorio (aosorio at cenditel.gob.ve)
