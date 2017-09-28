c47ba0bb6525d171439f0f73a27fe70706310079
rudmanmrrod <rudman22@hotmail.com>
2017-09-28 13:33:49 -0400

    Removido django filters del setting

--- gestor_consulta/settings.py
+++ gestor_consulta/settings.py
@@ -158,9 +158,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ## Configuraciones del rest framework
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': (
-        'django_filters.rest_framework.DjangoFilterBackend',
-    ),
     #'DEFAULT_PERMISSION_CLASSES': (
     #    'rest_framework.permissions.IsAuthenticated',
     #),
